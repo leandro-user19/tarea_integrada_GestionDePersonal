e872f333e647b1b2a0282b832e6a99ba12b23fd1
Leandro <leandro.jacho2003@gmail.com>
2026-05-26 21:09:45 -0500

    tercer avance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 
+public class Main {
 
-void main() {
+    public static void main(String[] args) {
 
-}
+        Scanner sc = new Scanner(System.in);
+
+        Sistema s = new Sistema();
+
+        int opcion = 0;
+
