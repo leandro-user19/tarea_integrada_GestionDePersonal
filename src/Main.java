6abaff8d2ab57df7b482f9c46ff2fac3297febac
Leandro <leandro.jacho2003@gmail.com>
2026-05-26 21:14:10 -0500

    cuarto avance

--- src/Main.java
+++ src/Main.java
@@ -10,3 +10,33 @@ public class Main {
 
         int opcion = 0;
 
+        do {
+            try {
+                System.out.println("\n===== CLINICA SALUD TOTAL =====");
+
+                System.out.println("1. Registrar medico");
+
+                System.out.println("2. Registrar administrativo");
+
+                System.out.println("3. Mostrar empleados");
+
+                System.out.println("4. Buscar por cedula");
+
+                System.out.println("5. Reemplazar informacion");
+
+                System.out.println("6. Eliminar registro");
+
+                System.out.println("7. Calcular pagos");
+
+                System.out.println("8. Mostrar estadisticas");
+
+                System.out.println("9. Salir");
+
+                System.out.print("Seleccione una opcion: ");
+
+                opcion = Integer.parseInt(sc.nextLine());
+
+                if (opcion < 1 || opcion > 9) {
+                    throw new Exception("Opcion invalida");
+                }
+
